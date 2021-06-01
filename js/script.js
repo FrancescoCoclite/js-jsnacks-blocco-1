8c5d36ad4fee189abc2d84c1ddaba2351f8a08a4
Francesco <francescococlite@gmail.com>
2021-06-01 16:13:56 +0200

    fix end

--- js/script.js
+++ js/script.js
@@ -2,52 +2,52 @@
 
 // L’utente inserisce due parole in successione, con due prompt. Il software stampa prima la parola più corta, poi la parola più lunga.
 
-// var primaParola = prompt('inserisci una parola');
-// var secondaParola = prompt('inserisci un\'altra parola');
+ var primaParola = prompt('inserisci una parola');
+ var secondaParola = prompt('inserisci un\'altra parola');
 
-// if (primaParola.length > secondaParola.length){
-//     document.getElementById("parola").innerHTML = 'La seconda parola è più corta ed è: ' + secondaParola;
+ if (primaParola.length > secondaParola.length){
+     document.getElementById("parola").innerHTML = 'La seconda parola è più corta ed è: ' + secondaParola;
 
-// }else if (primaParola.length < secondaParola.length){
-//     document.getElementById("parola").innerHTML = 'La prima parola è più corta ed è: ' + primaParola;
-// }else{
-//     document.getElementById("parola").innerHTML = 'Le due parole hanno la stessa lunghezza e sono: ' + primaParola + secondaParola;
+ }else if (primaParola.length < secondaParola.length){
+     document.getElementById("parola").innerHTML = 'La prima parola è più corta ed è: ' + primaParola;
+ }else{
+     document.getElementById("parola").innerHTML = 'Le due parole hanno la stessa lunghezza e sono: ' + primaParola + secondaParola;
 
-// }
+ }
 
 
-//  Esercizio N.2
+// Esercizio N.2
 
 // Il software deve chiedere per 5 volte all’utente di inserire un numero. Il programma stampa la somma di tutti i numeri   inseriti. Esegui questo programma in due versioni, con il for e con il while.
 
-// var somma = 0;
-// for (var i = 0; i<5; i++){
+ var somma = 0;
+ for (var i = 0; i<5; i++){
     
-//     var numero = parseInt(prompt('Inserisci un numero'));
-//     somma = somma + numero;
-//  }
-//  console.log(somma);
+     var numero = parseInt(prompt('Inserisci un numero'));
+     somma = somma + numero;
+  }
+  console.log(somma);
 
 
 // Esercizio N.3
 
 //Crea un array vuoto. Chiedi per 6 volte all’utente di inserire un numero, se è dispari inseriscilo nell’array
 
-// var array = []
+ var array = []
 
-// for (i=0; i<6; i++){
+ for (i=0; i<6; i++){
 
-//     var numero = parseInt(prompt('Inserisci un numero'));
-//     if (numero % 2 != 0){
+     var numero = parseInt(prompt('Inserisci un numero'));
+     if (numero % 2 != 0){
         
-//         array.push(numero)
-//     }
-// }
-// console.log(array);
+         array.push(numero)
+     }
+ }
+ console.log(array);
 
 // Esercizio N.4
 
-// In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
+//In un array sono contenuti i nomi degli invitati alla festa del grande Gatsby, chiedi all’utente il suo nome e comunicagli se può partecipare o no alla festa.
 
 var cognome = prompt('Inserisci il tuo cognome');
 
